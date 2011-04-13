4a259417972b32effd0654484ca0f4d6f0e9ac14
Rahul Kumar <rahulsinner@gmail.com>
2011-04-14 00:01:47 +0530

    Serving Sass and Coffeescript dynamically in development.

--- site_code/app.rb
+++ site_code/app.rb
@@ -1,9 +1,10 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require 'slim'
+require 'coffee-script'
+require 'sass'
 
 class App < Sinatra::Base
-
     configure do
         enable :sessions
         enable :dump_errors
@@ -14,13 +15,19 @@ class App < Sinatra::Base
     end
 
     configure :development do
+        get %r{/css/(.+)\.css} do |f|
+            sass :"sass/#{f}"
+        end 
+
+        get %r{/js/(.+)\.js} do |f|
+            coffee :"coffee/#{f}"
+        end 
     end
 
     helpers do
         include Rack::Utils
         alias_method :h, :escape_html
     end
-
 end
 
 require_relative 'models/init'
