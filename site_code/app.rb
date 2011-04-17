c0be3859b662a70c0ea3c475eb44fa01d73a082c
Rahul Kumar <rahulsinner@gmail.com>
2011-04-18 03:06:34 +0530

    Code refactoring.

--- site_code/app.rb
+++ site_code/app.rb
@@ -6,6 +6,7 @@ require 'sass'
 
 class App < Sinatra::Base
   STATIC_DIR = File.join(File.dirname(__FILE__), 'public')
+  EXPIRY_DURATION = 15552000
 
   configure do
     enable :dump_errors
@@ -14,14 +15,8 @@ class App < Sinatra::Base
   end
 
   def send_static_asset(sender, *args)
-    expires 15552000, :public
+    expires EXPIRY_DURATION, :public
     sender.call *args 
-  end
-
-  # Serve versioned image files if it falls back to the framework.
-  get %r(/img/ (.+?) (\..+?)? (\..+)$)x do |f, v, ext| 
-    expires 15552000, :public
-    send_file File.join(STATIC_DIR, "img", "#{f}#{ext}" )
   end
 
   configure :development do
@@ -34,7 +29,12 @@ class App < Sinatra::Base
     get %r(/js/ (.+?) \. (.+?\.)? js)x do |f, v|
       send_static_asset method(:coffee), :"coffee/#{f}"
     end
+  end
 
+  # Serve versioned image files if it falls back to the framework.
+  get %r(/img/ (.+?) (\..+?)? (\..+)$)x do |f, v, ext| 
+    send_static_asset method(:send_file),
+      File.join(STATIC_DIR, "img", "#{f}#{ext}" )
   end
 
   configure :production do
@@ -42,18 +42,18 @@ class App < Sinatra::Base
     # Statics should be served from webserver.
     # These rules are here basically for thin.
     get %r(/css/ (.+?) \. (.+?\.)? css)x do |f, v| 
-      expires 15552000, :public
-      send_file File.join(STATIC_DIR, "css", "#{f}.css" )
+      send_static_asset method(:send_file),
+        File.join(STATIC_DIR, "css", "#{f}.css" )
     end
 
     get %r(/js/ (.+?) \. (.+?\.)? js)x do |f, v| 
-      expires 15552000, :public
-      send_file File.join(STATIC_DIR, "js", "#{f}.js" )
+      send_static_asset method(:send_file),
+        File.join(STATIC_DIR, "js", "#{f}.js" )
     end
 
     get %r(/img/ (.+?) (\..+?)? (\..+)$)x do |f, v, ext| 
-      expires 15552000, :public
-      send_file File.join(STATIC_DIR, "img", "#{f}#{ext}" )
+      send_static_asset method(:send_file),
+        File.join(STATIC_DIR, "img", "#{f}#{ext}" )
     end
   end
 
